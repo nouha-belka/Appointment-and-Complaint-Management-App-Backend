202611d7ea49a21409b2261acf89bc01d54c9234
nouha-belka <nouhabelka2001@gmail.com>
2022-06-18 11:55:31 +0100

    update

--- dbconfig.php
+++ dbconfig.php
@@ -4,7 +4,7 @@ $db_name = "seaal";
 $db_user = "root";
 $db_pass = "";
 $db_host = "localhost";
-// $bdd = new PDO('mysql:host=localhost;dbname=seaal;charset=utf8;','root','');
+$bdd = new PDO('mysql:host=localhost;dbname=seaal;charset=utf8;','root','');
 $con = mysqli_connect($db_host,$db_user,$db_pass,$db_name);
 if(!$con){
     echo json_encode("connetion error");
